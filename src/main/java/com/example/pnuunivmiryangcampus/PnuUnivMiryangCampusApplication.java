6f3d021a1117c5110502bec01576569f8745c7a9
KimSehoon <shggm2000@gmail.com>
2024-02-16 03:39:44 +0900

    #1 feat: 스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    * Sprirng web
    * Spring data JPA
    * devtools
    * lombok
    * H2
    * MySql Driver
    * thymeleaf

---
+++ src/main/java/com/example/pnuunivmiryangcampus/PnuUnivMiryangCampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.pnuunivmiryangcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnuUnivMiryangCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnuUnivMiryangCampusApplication.class, args);
+    }
+
+}
