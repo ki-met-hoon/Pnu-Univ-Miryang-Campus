02c45ad4dc7364ed8d7882487cc9b3aa4e39f2f8
KimSehoon <shggm2000@gmail.com>
2024-03-05 00:59:01 +0900

    feat: Reservation class 구현

---
+++ src/main/java/com/example/pnuunivmiryangcampus/domain/Reservation.java
@@ -0,0 +1,39 @@
+package com.example.pnuunivmiryangcampus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class Reservation extends AuditingFields {
+
+    @Column(nullable = false)
+    private Long userAccountId;
+
+    @Column(nullable = false)
+    private Long librarySeatId;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endAt;
+
+    protected Reservation() {
+    }
+
+    private Reservation(Long userAccountId, Long librarySeatId, LocalDateTime startAt, LocalDateTime endAt) {
+        this.userAccountId = userAccountId;
+        this.librarySeatId = librarySeatId;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    public Reservation of(Long userAccountId, Long librarySeatId, LocalDateTime startAt, LocalDateTime endAt) {
+        return new Reservation(userAccountId, librarySeatId, startAt, endAt);
+    }
+}
