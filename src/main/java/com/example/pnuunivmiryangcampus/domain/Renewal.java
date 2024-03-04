cc1e8c4c4b01d07220038764efc941f5e8124341
KimSehoon <shggm2000@gmail.com>
2024-03-05 00:53:33 +0900

    feat: Renewal class 구현

---
+++ src/main/java/com/example/pnuunivmiryangcampus/domain/Renewal.java
@@ -0,0 +1,30 @@
+package com.example.pnuunivmiryangcampus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class Renewal extends AuditingFields {
+
+    @Column(nullable = false)
+    private Long reservationId;
+
+    @Column(nullable = false)
+    private int renewalCount;
+
+    protected Renewal() {
+    }
+
+    private Renewal(Long reservationId, int renewalCount) {
+        this.reservationId = reservationId;
+        this.renewalCount = renewalCount;
+    }
+
+    public Renewal of(Long reservationId, int renewalCount) {
+        return new Renewal(reservationId, renewalCount);
+    }
+}
