fe7c2275eb5e180acd2f4fa0f39795cdd2dc9bf8
KimSehoon <shggm2000@gmail.com>
2024-03-05 00:46:19 +0900

    feat: AuditingFields MappedSuperclass 구현
    - 해당 class를 다른 domain class에서 상속 받아 공통 필드 관리

---
+++ src/main/java/com/example/pnuunivmiryangcampus/domain/AuditingFields.java
@@ -0,0 +1,50 @@
+package com.example.pnuunivmiryangcampus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100, updatable = false)
+    private String createdBy;
+
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
