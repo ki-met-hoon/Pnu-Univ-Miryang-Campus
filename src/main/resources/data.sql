21373b890d76dbb06b87424549e1eacacd1520bd
KimSehoon <shggm2000@gmail.com>
2024-03-05 23:18:34 +0900

    #17 feat: 도서관 좌석 데이터 삽입
    - data.sql를 통해 도서관 좌석 데이터 삽입
    - 도서관 좌석은 존재하고 있는 값이기 때문에 미리 테이블에 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,107 @@
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (1, 1, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (2, 2, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (3, 3, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (4, 4, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (5, 5, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (6, 6, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (7, 7, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (8, 8, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (9, 9, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (10, 10, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (11, 11, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (12, 12, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (13, 13, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (14, 14, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (15, 15, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (16, 16, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (17, 17, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (18, 18, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (19, 19, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (20, 20, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (21, 21, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (22, 22, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (23, 23, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (24, 24, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (25, 25, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (26, 26, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (27, 27, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (28, 28, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (29, 29, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (30, 30, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (31, 31, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (32, 32, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (33, 33, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (34, 34, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (35, 35, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (36, 36, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (37, 37, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (38, 38, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (39, 39, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (40, 40, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (41, 41, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (42, 42, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (43, 43, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (44, 44, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (45, 45, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (46, 46, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (47, 47, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (48, 48, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (49, 49, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (50, 50, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (51, 51, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+insert into library_seat(id, seat_number,availability, created_by, created_at, modified_by, modified_at, is_deleted) values
+    (52, 52, '사용가능', '김세훈', CURRENT_TIMESTAMP, NULL, NULL, false);
+
+
+
