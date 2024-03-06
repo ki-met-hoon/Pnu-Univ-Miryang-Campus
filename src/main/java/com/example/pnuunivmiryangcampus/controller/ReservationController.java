de3320d5e3560a5516e86e12c15a6cd0644d00ef
KimSehoon <shggm2000@gmail.com>
2024-03-07 01:12:24 +0900

    #19 feat: ReservationController 구현
    - startAt과 그 시각에 4시간을 더한 endAt 변수 생성
    - getLibrarySeatId를 통해 librarySeatId 변수 생성
    - startAt, endAt 그리고 librarySeatId을 통해 reservationDto를 구현하고 saveReservation method를 통해 DB에 저장

---
+++ src/main/java/com/example/pnuunivmiryangcampus/controller/ReservationController.java
@@ -0,0 +1,35 @@
+package com.example.pnuunivmiryangcampus.controller;
+
+import com.example.pnuunivmiryangcampus.dto.ReservationDto;
+import com.example.pnuunivmiryangcampus.service.LibrarySeatService;
+import com.example.pnuunivmiryangcampus.service.ReservationService;
+import com.example.pnuunivmiryangcampus.util.ApiUtils;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/library/reservation")
+@RestController
+public class ReservationController {
+
+    private final ReservationService reservationService;
+    private final LibrarySeatService librarySeatService;
+
+    @PostMapping("/{seatNumber}")
+    public ResponseEntity<?> reservation(@PathVariable int seatNumber) {
+        LocalDateTime startAt = LocalDateTime.now();
+        LocalDateTime endAt = startAt.plusHours(4);
+
+        Long librarySeatId = librarySeatService.getLibrarySeatId(seatNumber);
+        //kakao 로그인 서비스 구현 후 1L을 security에서 받아온 ID 값으로 변경해야함
+        ReservationDto reservationDto = ReservationDto.of(1L, librarySeatId, startAt, endAt);
+        reservationService.saveReservation(reservationDto);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+}
