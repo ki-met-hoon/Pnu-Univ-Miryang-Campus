e532eceb4abaf7a680f886b9d1b64698d9a05031
KimSehoon <shggm2000@gmail.com>
2024-03-07 23:59:25 +0900

    #26 refactor: 요청 값 변경으로 인한 method 수정
    - 요청 값 seatNumber -> seatId 변경
    - seatNumber로 seatId를 가져오는 로직 삭제

--- src/main/java/com/example/pnuunivmiryangcampus/controller/ReservationController.java
+++ src/main/java/com/example/pnuunivmiryangcampus/controller/ReservationController.java
@@ -1,7 +1,6 @@
 package com.example.pnuunivmiryangcampus.controller;
 
 import com.example.pnuunivmiryangcampus.dto.ReservationDto;
-import com.example.pnuunivmiryangcampus.service.LibrarySeatService;
 import com.example.pnuunivmiryangcampus.service.ReservationService;
 import java.net.URI;
 import java.time.LocalDateTime;
@@ -18,16 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     private final ReservationService reservationService;
-    private final LibrarySeatService librarySeatService;
 
-    @PostMapping("/{seatNumber}")
-    public ResponseEntity<Void> reservation(@PathVariable int seatNumber) {
+    @PostMapping("/{seatId}")
+    public ResponseEntity<Void> reservation(@PathVariable Long seatId) {
         LocalDateTime startAt = LocalDateTime.now();
         LocalDateTime endAt = startAt.plusHours(4);
 
-        Long librarySeatId = librarySeatService.getLibrarySeatId(seatNumber);
         //kakao 로그인 서비스 구현 후 1L을 security에서 받아온 ID 값으로 변경해야함
-        ReservationDto reservationDto = ReservationDto.of(1L, librarySeatId, startAt, endAt);
+        ReservationDto reservationDto = ReservationDto.of(1L, seatId, startAt, endAt);
         Long reservationId = reservationService.saveReservationAndReturnId(reservationDto);
 
         return ResponseEntity.created(URI.create("/library/reservation/" + reservationId)).build();
