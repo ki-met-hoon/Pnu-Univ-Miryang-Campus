a8f7ced6665d32fbc88b0f76dc90f5c39d189e3a
KimSehoon <shggm2000@gmail.com>
2024-03-08 00:05:30 +0900

    #26 refactor: API 변경으로 인한 Id field 추가

--- src/main/java/com/example/pnuunivmiryangcampus/dto/response/LibrarySeatResponse.java
+++ src/main/java/com/example/pnuunivmiryangcampus/dto/response/LibrarySeatResponse.java
@@ -4,16 +4,18 @@ import com.example.pnuunivmiryangcampus.dto.LibrarySeatDto;
 import java.io.Serializable;
 
 public record LibrarySeatResponse(
+        Long id,
         int seatNumber,
         String availability
 ) implements Serializable {
 
-    public static LibrarySeatResponse of(int seatNumber, String availability) {
-        return new LibrarySeatResponse(seatNumber, availability);
+    public static LibrarySeatResponse of(Long id, int seatNumber, String availability) {
+        return new LibrarySeatResponse(id, seatNumber, availability);
     }
 
     public static LibrarySeatResponse from(LibrarySeatDto dto) {
         return new LibrarySeatResponse(
+                dto.id(),
                 dto.seatNumber(),
                 dto.availability()
         );
