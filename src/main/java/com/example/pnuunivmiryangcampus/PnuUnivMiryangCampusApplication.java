28de1060606f3b5798074e25a42bec56e6366519
KimSehoon <shggm2000@gmail.com>
2024-03-09 15:09:40 +0900

    #29 chore: root package @EnableFeignClients 추가

--- src/main/java/com/example/pnuunivmiryangcampus/PnuUnivMiryangCampusApplication.java
+++ src/main/java/com/example/pnuunivmiryangcampus/PnuUnivMiryangCampusApplication.java
@@ -3,9 +3,11 @@ package com.example.pnuunivmiryangcampus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class PnuUnivMiryangCampusApplication {
 
