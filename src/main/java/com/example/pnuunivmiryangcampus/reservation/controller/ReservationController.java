f23a56388a593f50156c766d61f8bfe4c6509934
KimSehoon <shggm2000@gmail.com>
2024-03-21 23:40:45 +0900

    #19 refactor: 예약 생성 로직 리팩토링
    - kakao 로그인 서비스 및 시큐리티 구현 후 하드 코딩된 user_account ID 값을 securtiy에서 받아온 ID 값으로 변경
    - saveReservationAndReturnId method 삭제

--- src/main/java/com/example/pnuunivmiryangcampus/reservation/controller/ReservationController.java
+++ src/main/java/com/example/pnuunivmiryangcampus/reservation/controller/ReservationController.java
@@ -1,15 +1,20 @@
 package com.example.pnuunivmiryangcampus.reservation.controller;
 
+import com.example.pnuunivmiryangcampus.config.security.CustomUserDetails;
 import com.example.pnuunivmiryangcampus.reservation.dto.ReservationDto;
 import com.example.pnuunivmiryangcampus.reservation.service.ReservationService;
-import java.net.URI;
-import java.time.LocalDateTime;
+import com.example.pnuunivmiryangcampus.userAccount.UserAccount;
+import com.example.pnuunivmiryangcampus.userAccount.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RequestMapping("/library/reservation")
@@ -17,14 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final UserAccountRepository userAccountRepository;
 
     @PostMapping("/{seatId}")
-    public ResponseEntity<Void> reservation(@PathVariable Long seatId) {
+    public ResponseEntity<Void> reservation(@PathVariable Long seatId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         LocalDateTime startAt = LocalDateTime.now();
         LocalDateTime endAt = startAt.plusHours(4);
+        UserAccount findUser = userAccountRepository.findBySub(customUserDetails.getPassword()).orElseThrow();
 
-        //kakao 로그인 서비스 구현 후 1L을 security에서 받아온 ID 값으로 변경해야함
-        ReservationDto reservationDto = ReservationDto.of(1L, seatId, startAt, endAt);
+        ReservationDto reservationDto = ReservationDto.of(findUser.getId(), seatId, startAt, endAt);
+        reservationService.saveReservation(reservationDto);
 
         return ResponseEntity.created(URI.create("/library/reservation")).build();
     }
