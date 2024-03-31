36503bde452dfdd1ced60dc5bcb342b9cf11dca9
KimSehoon <shggm2000@gmail.com>
2024-03-31 15:35:12 +0900

    #45 feat: 세부 에러 코드 클래스 구현

---
+++ src/main/java/com/example/pnuunivmiryangcampus/support/exception/InternalException.java
@@ -0,0 +1,8 @@
+package com.example.pnuunivmiryangcampus.support.exception;
+
+public class InternalException extends BusinessException {
+
+    public InternalException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/pnuunivmiryangcampus/support/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.pnuunivmiryangcampus.support.exception;
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
